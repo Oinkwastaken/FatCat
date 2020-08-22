143a5a2c9acff81d9c153744df7a5506529b2399
Oinkwastaken <66695682+Oinkwastaken@users.noreply.github.com>
2020-08-22 11:48:44 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,20 +16,38 @@ var milk
 var button1
 var button
 var milkImg
+var bedroom
+var garden
+var bathroom
 function preload()
 {
   happycat=loadImage("images/happycat.png")
   madcat=loadImage("images/angry cat.png")
   fatcat=loadImage("images/fatcat.png")
+  bedroom=loadImage("images/bedroom.jpg")
+  garden=loadImage("images/garden.jpg")
+  bathroom=loadImage("images/bathroom.jpg")
+  
 }
 
 function setup() {
   createCanvas(500, 500);
   database=firebase.database()
+  back1 = createSprite(250,250,50,50)
+  back1.addImage("bed",bedroom)
+  back1.visible=false
+  back2 = createSprite(250,250,50,50)
+  back2.addImage("garden",garden)
+  back2.visible=false
+  back3 = createSprite(250,250,50,50)
+  back3.addImage("bath",bathroom)
+  back3.visible=false
   pet1 = createSprite(250,250,50,50)
   pet1.addImage("mad",madcat)
+  pet1.visible=false
   pet2 = createSprite(250,250,50,50)
   pet2.addImage("happy",happycat)
+  pet2.visible=false
   pet3 = createSprite(250,250,50,50)
   pet3.addImage("fat",fatcat)
   pet3.visible=false
@@ -49,9 +67,11 @@ function draw() {
   text("press the up arrow to feed",100,50)
   text("Don't make him to fat though",100,75)
   if(a<5){
+   back2.visible=true
+    back3.visible=false
     pet2.visible=true
     pet3.visible=false
-   
+    
   }
 button.mousePressed(function(){
   console.log(a)
@@ -65,17 +85,22 @@ button.mousePressed(function(){
     console.log(a)
     })
   if(a<0){
+    back1.visible=true
+    back2.visible=false
     pet1.visible=true
     pet2.visible=false
-   
+    
   }
   if(a>5){
+    
+    back3.visible=true
+    back2.visible=false
     pet3.visible=true
     pet2.visible=false
    
   }
   text("you have "+ foodS+ " food left",100,100)
-  
+   
   
   drawSprites();
   a=a-0.01
